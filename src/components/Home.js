6fca6aa9c0f5ca6e4ad66f26e1a18c1a57782a49
Bruno Tumedei <brunotumedei@gmail.com>
2024-05-23 23:26:41 +0200

    Add homepage sections

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,21 @@
 import DocumentTitle from "./DocumentTitle";
+import {Link} from "react-router-dom";
 
 export default function Home() {
     DocumentTitle("8bitmd")
     return (
-        <section>
-            <h2>Je suis <span className={"decoration"}>8bitmd</span>, Bruno Igal Tumedei dans la vraie vie.</h2>
-            <p>Passionné par le hardware et l'informatique en général, le développement web et les langues.</p>
+        <section className={"accueil"}>
+            <h2>👋 Je suis <span className={"decoration"}>8bitmd</span>, Bruno Igal Tumedei dans la vraie vie.</h2>
+            <p>Passionné par la couleur jaune et tout ce qui est numérique, en incluant le hardware, je me suis finalement spécialisé dans le développement web.</p>
+            <p>Je suis également passionné par les langues, ce qui m'a permis d'étudier en Angleterre et de décrocher une Licence en Langues Étrangères Appliquées, tout en découvrant de nouvelles cultures et en apprenant à communiquer efficacement dans un monde globalisé.</p>
+            <p>Ici vous pourrez découvrir la plupart des projets sur lesquels j'ai travaillé, ou je travaille toujours.</p>
+            <p>Pour en savoir un peu plus sur moi, <Link to={"#"}>c'est par là</Link>.</p>
             <h2>Mes projets</h2>
-            <h2>Éducation</h2>
+            <p>Pour avoir un aperçu des projets sur lesquels j'ai travaillé, ou mes projets en cours, vous pouvez <Link to={"/projets"}>cliquer ici</Link>.</p>
+            <p></p>
             <h2>Contact</h2>
+            <p>Il n'y a rien de plus simple pour me contacter.</p>
+            <p><a href={"https://www.linkedin.com/in/brunotumedei/"}>Cliquez ici</a> pour accéder à mon LinkedIn, mais si vous préférez les e-mails <a href={"mailto:bruno@tumedei.dev"}>c'est par là</a> (bruno@tumedei.dev).</p>
         </section>
     )
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ export default function Header() {
                 <Link to={"/projets"}><span aria-hidden={"true"}>[</span>projets<span aria-hidden={"true"}>]</span></Link>
                 <a href={"https://github.com/8bitmd"}><span aria-hidden={"true"}>[</span>github<span aria-hidden={"true"}>]</span></a>
                 <a href={"https://www.linkedin.com/in/brunotumedei/"}><span aria-hidden={"true"}>[</span>linkedin<span aria-hidden={"true"}>]</span></a>
-                <a href={"mailto:bruno@tumedei.dev"}><span aria-hidden={"true"}>[</span>email<span aria-hidden={"true"}>]</span></a>
+                <a href={"mailto:bruno@tumedei.dev"}><span aria-hidden={"true"}>[</span>e-mail<span aria-hidden={"true"}>]</span></a>
             </nav>
         </header>
     )
