25388454b6cf49d605ad4b5105e74f1a5bbf35b7
Bruno Tumedei <brunotumedei@gmail.com>
2024-05-29 16:45:12 +0200

    Remove placeholder

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,6 @@ export default function Home() {
             <p>Passionné par la couleur jaune et tout ce qui est numérique, en incluant le hardware, je me suis finalement spécialisé dans le développement web.</p>
             <p>Je suis également passionné par les langues, ce qui m'a permis d'étudier en Angleterre et de décrocher une Licence en Langues Étrangères Appliquées, tout en découvrant de nouvelles cultures et en apprenant à communiquer efficacement dans un monde globalisé.</p>
             <p>Ici vous pourrez découvrir la plupart des projets sur lesquels j'ai travaillé, ou je travaille toujours.</p>
-            <p>Pour en savoir un peu plus sur moi, <Link to={"#"}>c'est par là</Link> (en construction).</p>
             <h2>Mes projets</h2>
             <p>Pour avoir un aperçu des projets sur lesquels j'ai travaillé, ou mes projets en cours, vous pouvez <Link to={"/projets"}>cliquer ici</Link>.</p>
             <p></p>
