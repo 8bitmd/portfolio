bc32c3ca63ed18fc6b3829f33d32d8b769809edd
Bruno Tumedei <brunotumedei@gmail.com>
2024-05-29 21:43:50 +0200

    Add home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,26 @@ export default function Home() {
             <p>Passionné par la couleur jaune et tout ce qui est numérique, en incluant le hardware, je me suis finalement spécialisé dans le développement web.</p>
             <p>Je suis également passionné par les langues, ce qui m'a permis d'étudier en Angleterre et de décrocher une Licence en Langues Étrangères Appliquées, tout en découvrant de nouvelles cultures et en apprenant à communiquer efficacement dans un monde globalisé.</p>
             <p>Ici vous pourrez découvrir la plupart des projets sur lesquels j'ai travaillé, ou je travaille toujours.</p>
+            <h2>Compétences et parcours</h2>
+            <h3>Formation Intégrateur Web - <em>OpenClassrooms</em></h3>
+            <ul>
+                <li>Langages front-end
+                <ul>
+                    <li>HTML, Sass, JavaScript</li>
+                </ul></li>
+                <li>Frameworks JavaScript
+                    <ul>
+                        <li>React (+ Router, Redux)</li>
+                    </ul></li>
+            </ul>
+            <h3>Licence LEA Ang/Esp - <em>Uni. Paris Cité, Uni. of Birmingham</em></h3>
+            <h3>Langues</h3>
+            <ul>
+                <li>Anglais - bilingue</li>
+                <li>Français - bilingue</li>
+                <li>Espagnol - Langue maternelle</li>
+                <li>Italien - langue maternelle</li>
+            </ul>
             <h2>Mes projets</h2>
             <p>Pour avoir un aperçu des projets sur lesquels j'ai travaillé, ou mes projets en cours, vous pouvez <Link to={"/projets"}>cliquer ici</Link>.</p>
             <p></p>
