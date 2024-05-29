1e7fc0e4202a75e67ef6fd920f34bd4238408903
Bruno Tumedei <brunotumedei@gmail.com>
2024-05-29 17:02:40 +0200

    Add noreferrer

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,8 +8,8 @@ export default function Header() {
             </Link>
             <nav>
                 <Link to={"/projets"}><span aria-hidden={"true"}>[</span>projets<span aria-hidden={"true"}>]</span></Link>
-                <a href={"https://github.com/8bitmd"} target={"_blank"}><span aria-hidden={"true"}>[</span>github<span aria-hidden={"true"}>]</span></a>
-                <a href={"https://www.linkedin.com/in/brunotumedei/"} target={"_blank"}><span aria-hidden={"true"}>[</span>linkedin<span aria-hidden={"true"}>]</span></a>
+                <a href={"https://github.com/8bitmd"} target={"_blank"} rel="noreferrer"><span aria-hidden={"true"}>[</span>github<span aria-hidden={"true"}>]</span></a>
+                <a href={"https://www.linkedin.com/in/brunotumedei/"} target={"_blank"} rel="noreferrer"><span aria-hidden={"true"}>[</span>linkedin<span aria-hidden={"true"}>]</span></a>
                 <a href={"mailto:bruno@tumedei.dev"}><span aria-hidden={"true"}>[</span>e-mail<span aria-hidden={"true"}>]</span></a>
             </nav>
         </header>
