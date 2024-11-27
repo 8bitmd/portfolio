dc6b6eba96d813710dcd8d85e52913f34493d47a
Bruno Igal Tumedei - 8bitmd <bruno@tumedei.dev>
2024-11-27 03:44:29 +0100

    Fix footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,10 +1,9 @@
 const currentYear = new Date().getFullYear()
-document.getElementById("year").innerHTML = currentYear
 
 export default function Footer() {
     return (
         <footer>
-            <p>&copy; <span id="year"></span> Bruno Igal Tumedei - 8bitmd</p>
+            <p>&copy; {currentYear} Bruno Igal Tumedei - 8bitmd</p>
         </footer>
     )
 }
