48d086308c13d311b50c25c3476b5d0f932fb6f4
Bruno Igal Tumedei <bruno@tumedei.dev>
2025-05-31 02:11:11 +0200

    Add banner for work in progress portfolio v2

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import {Outlet} from "react-router-dom";
 import Footer from "./Footer";
+import WorkInProgress from "../WorkInProgress";
 
 export default function Layout() {
     return (
         <div>
+            <WorkInProgress />
             <Header />
             <Outlet />
             <Footer />
---
+++ src/components/WorkInProgress.js
@@ -0,0 +1,7 @@
+export default function WorkInProgress() {
+    return (
+        <div className={"work-in-progress"}>
+            <p>⚠️ Ce portfolio ne sera plus maintenu. Une version 2 complètement repensée est en cours de création.</p>
+        </div>
+    )
+}
